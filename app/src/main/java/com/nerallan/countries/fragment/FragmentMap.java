d0643a3c28f62c2b63ca4e1130afd744e2edd409
Nerallan <budkomaximidu@gmail.com>
2018-09-20 19:53:35 +0300

    google map in fragment added

--- app/src/main/java/com/nerallan/countries/fragment/FragmentMap.java
+++ app/src/main/java/com/nerallan/countries/fragment/FragmentMap.java
@@ -1,13 +1,24 @@
 package com.nerallan.countries.fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.nerallan.countries.R;
 
 /**
@@ -15,7 +26,10 @@ import com.nerallan.countries.R;
  */
 
 public class FragmentMap extends Fragment{
+
     View v;
+    private SupportMapFragment mapFragment;
+    private GoogleMap mMap;
 
     public FragmentMap(){
 
@@ -25,6 +39,33 @@ public class FragmentMap extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.map_fragment, container, false);
+
+        // Remove old code
+        // SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        // don't recreate fragment everytime ensure last map location/state are maintained
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    LatLng latLng = new LatLng(53.7169415, 27.9775789);
+                    googleMap.addMarker(new MarkerOptions().position(latLng)
+                            .title("Belarus"));
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                        return;
+                    }
+                 mMap.setMyLocationEnabled(true);
+                }
+            });
+        }
+
+        // R.id.map is a FrameLayout, not a Fragment
+        getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
+
+        //return view;
         return v;
     }
 }
